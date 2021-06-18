3d789896eae8fd2c91eb7e48c2b189870bc89fbc
JR622445 <85941738+JR622445@users.noreply.github.com>
2021-06-18 12:36:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -21,12 +21,13 @@ ThinkTravel = 'A travel agency in Winnipeg that provide affordable prices for to
 EleganceBeard= 'A barber shop which can be contacted here elegancebeard.com' #Store Kitchener
 Dignitii = "An online sportwear company for Muslim women in the Kitchener and Waterloo area. found at https://www.dignitii.com." #waterloo online sportswear
 
-print('Welcome to our program. We hope that it will be very helpful in finding Muslim owned business across Canada. \n \nTo get started, please type a number.\n \n 1. Search a business by name, if you already know a business in our program. \n 2. Search by business type. \n 3. View the whole list of businesses in alphabetical order. \n 4. Add a business to the list.')
+print('Welcome to our program. We hope that it will be very helpful in finding Muslim owned business across Canada. \n \nTo get started, please type a number.\n \n 1. Search a business by city. \n 2. Search by business type. \n 3. View the whole list of businesses in alphabetical order. \n 4. Add a business to the list.')
 
 print('''To exit the program now, please type 'Exit'.\n''')
-myList = ['Kebab Halal Pizza', 'Star Biryani', 'Al-Karam Sweets', 'Apna Farm', "Shabi's", 'Tun Travel', 'Wild Fish', 'Autorebex', 'Wealth Managment', 'Immigration', 'NageenabyNaureen', 'Mann Layers', 'Gazelle Media', 'Graphic Design', 'Quran Program', 'Children Clothing', 'Halal Commerce', 'Wave Fitness', 'Think Travel', 'EleganceBeard', 'Dignitii']
 
-#Joshua: lines 30-38
+myList = ['Kebab Halal Pizza', 'Star Biryani', 'Al-Karam Sweets', 'Apna Farm', "Shabi's", 'Tun Travel', 'WildFish', 'Autorebex', 'Wealth Managment', 'Immigration', 'NageenabyNaureen', 'Mann Layers', 'Gazelle Media', 'Graphic Design', 'Quran Program', 'Children Clothing', 'Halal Commerce', 'Wave Fitness', 'Think Travel', 'EleganceBeard', 'Dignitii']
+
+#Joshua: lines 30-72
 newInput = str(input())
 
 while newInput != '1' and newInput != '2' and newInput != '3' and newInput != '4' and newInput != 'Exit':
@@ -34,16 +35,129 @@ while newInput != '1' and newInput != '2' and newInput != '3' and newInput != '4
   newInput = str(input())
 
 if newInput == '1':
-  print('Please search a business by name to know more about it.')
+  print('Please search a city by name to know more about it.\n**The GTA includes Toronto, Mississauga, Kingston and Waterloo\nGTA\nKitchener\nVancouver\nCalgary\nOttawa\nHalifax\nEdmonton')
+  gta = [myList[0], myList[1], myList[2], myList[3], myList[4]]
+  kitchener = [myList[20], myList[14], myList[15], myList[19], myList[16]]
+  vancouver = [myList[17]]
+  calgary = [myList[5], myList[6]]
+  ottawa = [myList[7], myList[13]]
+  halifax = [myList[9], myList[10]]
+  edmonton = [myList[8]]
   newInputTwo = str(input())
+  print()
+  while newInputTwo != 'GTA' and newInputTwo != 'Kitchener' and newInputTwo != 'Vancouver' and newInputTwo != 'Calgary' and newInputTwo != 'Ottawa' and newInputTwo != 'Halifax' and newInputTwo != 'Edmonton':
+    print('Invalid city please try again.')
+    newInputTwo = str(input())
+  
+  if newInputTwo == 'GTA':
+    for i in range(0,len(gta),1):
+      print(gta[i])
+    print('Please search a business from the GTA.')
+    gtaInput = str(input())
+    
+    while gtaInput != 'Kebab Halal Pizza' and gtaInput != 'Star Biryani' and gtaInput != 'Al-Karam Sweets' and gtaInput != 'Apna Farm' and gtaInput != "Shabi's":
+      print('Invalid business in the GTA please try again.')
+      gtaInput = str(input())
+    if gtaInput == 'Kebab Halal Pizza':
+      print(KebabHalalPizza)
+    elif gtaInput == 'Star Biryani':
+      print(StarBiryani)
+    elif gtaInput == 'Al-Karam Sweets':
+      print(AlKaramSweets)
+    elif gtaInput == 'Apna Farm':
+      print(ApnaFarm)
+    elif gtaInput == "Shabi's":
+      print(Shabis)
+ 
+
+  elif newInputTwo == 'Kitchener':
+    for item in kitchener:
+      print(item)
+    print('Please search a business from Kitchener.')
+    kitchenerInput = str(input())
+    
+    while kitchenerInput != 'Dignitii' and kitchenerInput != 'Quran Program' and kitchenerInput != 'EleganceBeard' and kitchenerInput != 'HalaCommerce' and kitchenerInput != "Children Clothing":
+      print('Invalid business in Kitchener please try again.')
+      kitchenerInput = str(input())
+    if kitchenerInput == 'Dignitii':
+      print(Dignitii)
+    elif kitchenerInput == 'Quran Program':
+      print(QuranProgram)
+    elif kitchenerInput == 'EleganceBeard':
+      print(EleganceBeard)
+    elif kitchenerInput == 'Hala Commerce':
+      print(HalaCommerce)
+    elif kitchenerInput == "Children Clothing":
+      print(ChildrenClothing)
+
+  elif newInputTwo == 'Vancouver':
+    for i in range(0,len(vancouver),1):
+      print(vancouver[i])
+    print('Please search a business from Vancouver.')
+    vancouverInput = str(input())
+    while vancouverInput != 'Wave Fitness':
+      print('Invalid business in Vancouver please try again.')
+      vancouverInput = str(input())
+    if vancouverInput == 'Wave Fitness':
+      print(WaveFitness)
+
+  elif newInputTwo == 'Calgary':
+    for i in range(0,len(calgary),1):
+      print(calgary[i])
+    print('Please search a business from Calgary.')
+    calgaryInput = str(input())
+    while calgaryInput != 'Tun Travel' and calgaryInput != 'WildFish':
+      print('Invalid business in Vancouver please try again.')
+      calgaryInput = str(input())
+    if calgaryInput == 'Tun Travel':
+      print(TunTravel)
+    elif calgaryInput == 'WildFish':
+      print(WildFish)
 
 
-#Kianoush: lines 42-78
+  elif newInputTwo == 'Ottawa':
+    for i in range(0,len(ottawa),1):
+      print(ottawa[i])
+    print('Please search a business from Ottawa.')
+    ottawaInput = str(input())
+    while ottawaInput != 'Autorebex' and ottawaInput != 'Graphic Design':
+      print('Invalid business in Ottawa please try again.')
+      ottawaInput = str(input())
+    if ottawaInput == 'Autorebex':
+      print(Autorebex)
+    elif calgaryInput == 'Graphic Design':
+      print(GraphicDesign)
+
+  elif newInputTwo == 'Halifax':
+    for i in range(0,len(halifax),1):
+      print(halifax[i])
+    print('Please search a business from Ottawa.')
+    halifaxInput = str(input())
+    while halifaxInput != 'Immigration' and halifaxInput != 'NageenabyNaureen':
+      print('Invalid business in Ottawa please try again.')
+      halifaxInput = str(input())
+    if halifaxInput == 'Immigration':
+      print(Immigration)
+    elif halifaxInput == 'NageenabyNaureen':
+      print(NageenabyNaureen)
+
+  elif newInputTwo == 'Edmonton':
+    for i in range(0,len(edmonton),1):
+      print(edmonton[i])
+    print('Please search a business from Edmonton.')
+    edmontonInput = str(input())
+    while edmontonInput != 'WealthManagment':
+      print('Invalid business in Edmonton please try again.')
+      edmontonInput = str(input())
+    if edmontonInput == 'Wealth Managment':
+      print(WealthManagment)
+
+#Kianoush: lines 155-273
 elif newInput == '2':
   print('Please type any of the following categories to see its businesses.')
   print('Food\nStore\nTransportation\nMedia\nOther ')
   print()
-  food = [myList[0],myList[1],myList[2],myList[3],] 
+  food = [myList[0],myList[1],myList[2],myList[3]] 
   Store= [myList[4],myList[6],myList[7],myList[10],myList[13],myList[15],myList[16],myList[19],myList[20]]
   transportation = [myList[5], myList[9]]
   media = [myList[12]]
@@ -59,26 +173,107 @@ elif newInput == '2':
     print("Food related businesses:")
     for i in range(0,len(food),1):
       print(food[i])
+    foodInput= str(input)
+    print()
+    while foodInput != 'Al-Karam Sweets' and foodInput != 'Apna Farm' and foodInput != 'Kebab Halal Pizza' and foodInput != 'Star Biryani': 
+      foodInput= str(input())
+      print("Invalid bussines try again:")
+    if foodInput == 'Al-Karam Sweets':
+      print(AlKaramSweets) 
+    elif foodInput == 'Apna Farm':
+      print(ApnaFarm)
+    elif foodInput == 'Kebab Halal Pizza':
+      print(KebabHalalPizza)
+    elif foodInput == 'Star Biryani':
+      print(StarBiryani)
+
+
   elif typeInput == 'Store':
     print()
     print("Stores:")
     for i in range(0,len(Store),1):
       print(Store[i])
+      
+    storeInput= str(input())
+    print()
+    while storeInput != "Shabi's" and storeInput != 'WildFish' and storeInput != "Autorebex" and storeInput != "NageenabyNaureen" and storeInput != "Graphic Design" and storeInput != "Children Clothing" and storeInput != "Halal Commerce" and storeInput != "EleganceBeard" and storeInput != "Dignitii":
+      print("Invalid business try again: ")
+      storeInput= str(input())
+      
+    if storeInput == "Shabi's":
+      print(Shabis)
+    elif storeInput == 'WildFish':
+      print(WildFish)
+    elif storeInput == 'Autorebex':
+      print(Autorebex)
+    elif storeInput== ' NageenabyNaureen':
+      print(NageenabyNaureen)
+    elif storeInput == 'Graphic Design':
+      print(GraphicDesign)
+    elif storeInput == 'Children Clothing':
+      print(ChildrenClothing)
+    elif storeInput== ' Halal Commerce':
+      print(HalaCommerce)
+    elif storeInput ==' EleganceBeard':
+      print(EleganceBeard)
+    elif storeInput == ' Dignitii':
+      print(Dignitii)
+
+    
   elif typeInput == 'Transportation':
     print('Transportation related businesses:')
     for i in range(0,len(transportation),1):
       print(transportation[i])
+    
+    transportationInput = input()
+    print()
+    while transportationInput != 'Tun Travel' and transportationInput != 'Immigration':
+      print("Invalid business try again:")
+      transportationInput = input()
+    if transportationInput == 'Tun Travel':
+      print(TunTravel)
+    elif transportationInput == 'Immigration':
+      print(Immigration)
+      
+    
+
   elif typeInput == 'Media':
     print('Media related businesses:')
     for i in range(0,len(media),1):
       print(media[i])      
+
+    mediaInput = input()
+    print()
+    while mediaInput != 'Gazelle Media':
+      print('Invalid business try again:')
+      mediaInput = input() 
+    if mediaInput == 'Gazelle Media':
+      print (GazelleMedia)
+
+
+      
   elif typeInput == 'Other':
     print('Other related businesses:')
     for i in range(0,len(other),1):
       print(other[i])    
-      
+    otherInput = input()
+    print()
+    while otherInput != 'Wealth Managment ' and otherInput != 'Mann Layers' and otherInput !=' Quran Program' and otherInput != 'Wave Fitness' and otherInput != 'Think Travel':
+      print("Invalid business try again:")
+      otherInput= input() 
+    if otherInput == 'Wealth Managment':
+      print(WealthManagment)
+    elif otherInput =='Mann Layers':
+      print(MannLawyers)
+    elif otherInput== 'Quran Program':
+      print(QuranProgram)
+    elif otherInput == 'Wave Fitness':
+      print(WaveFitness)
+    elif otherInput =='Think Travel':
+      print(ThinkTravel)
+    
 
-#Joshua: lines 82-150
+#Joshua: lines 276-330
 elif newInput == '3':
   print('Please search one of the businesses to view more information about it.\n')
   for i in range(0, len(myList), 1):
@@ -134,11 +329,19 @@ elif newInput == '3':
   elif newInputThree == 'Apna Farm':
     print(ApnaFarm)
 
+# Joshua made structure and Kianoush added details
 elif newInput == '4':
   print('Add a business to the list and press enter.')
   newInputFive = str(input())
   print('Please add a description for this business.')
   newInputSix = str(input())
+  print('In which Category is your business in:')
+  Category = ['Food', 'Store', 'Transportation', 'Media', 'Other']
+  print("\n".join(Category))
+  newInputSeven = input()
+  while newInputSeven != 'Food' and newInputSeven != 'Store' and newInputSeven != 'Transportation' and newInputSeven != 'Media' and newInputSeven != 'Other':
+    print("That Category does not exist try again:")
+    newInputSeven= input()
   print(newInputFive + ' - ' + newInputSix)
   stringList = []
   stringList.append(newInputFive)
@@ -147,7 +350,6 @@ elif newInput == '4':
   mergedList.sort()
   for item in mergedList:
     print(item)
-
 
 elif newInput == 'Exit':
   print('Thank you for using our program.')
